88891e5a7314099ad5985edfe4e867c35d3158a4
Tom Ekstrom <tom@taiste.fi>
2016-02-16 15:17:34 +0200

    Install.ps updated

--- nuget/tools/net40/install.ps1
+++ nuget/tools/net40/install.ps1
@@ -1,79 +1,35 @@
 param($installPath, $toolsPath, $package, $project)
 
 
-function RemoveForceProjectLevelHack($project)
-{
-    Write-Host "RemoveForceProjectLevelHack" 
-	Foreach ($item in $project.ProjectItems) 
-	{
-		if ($item.Name -eq "Fody_ToBeDeleted.txt")
-		{
-			$item.Delete()
-		}
-	}
-}
-
-function FlushVariables()
-{
-    Write-Host "Flushing environment variables"
-    $env:FodyLastProjectPath = ""
-    $env:FodyLastWeaverName = ""
-    $env:FodyLastXmlContents = ""
-}
-
 function Update-FodyConfig($addinName, $project)
 {
-	Write-Host "Update-FodyConfig" 
     $fodyWeaversPath = [System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName($project.FullName), "FodyWeavers.xml")
+ 
+    if (!(Test-Path ($fodyWeaversPath)))
+    {
+        return
+    }   	
 
-	$FodyLastProjectPath = $env:FodyLastProjectPath
-	$FodyLastWeaverName = $env:FodyLastWeaverName
-	$FodyLastXmlContents = $env:FodyLastXmlContents
+    Write-Host "Caching variables for possible update"
+	$env:FodyLastProjectPath = $project.FullName
+	$env:FodyLastWeaverName = $addinName
+	$env:FodyLastXmlContents = [IO.File]::ReadAllText($fodyWeaversPath)
 	
-	if (
-		($FodyLastProjectPath -eq $project.FullName) -and 
-		($FodyLastWeaverName -eq $addinName))
-	{
-        Write-Host "Upgrade detected. Restoring content for $addinName"
-		[System.IO.File]::WriteAllText($fodyWeaversPath, $FodyLastXmlContents)
-        FlushVariables
-		return
-	}
-	
-    FlushVariables
 
     $xml = [xml](get-content $fodyWeaversPath)
 
     $weavers = $xml["Weavers"]
     $node = $weavers.SelectSingleNode($addinName)
 
-    if (-not $node)
+    if ($node)
     {
-        Write-Host "Appending node"
-        $newNode = $xml.CreateElement($addinName)
-        $weavers.AppendChild($newNode)
+        Write-Host "Removing node from FodyWeavers.xml"
+        $weavers.RemoveChild($node)
     }
 
     $xml.Save($fodyWeaversPath)
 }
 
-function Fix-ReferencesCopyLocal($package, $project)
-{
-    Write-Host "Fix-ReferencesCopyLocal $($package.Id)"
-    $asms = $package.AssemblyReferences | %{$_.Name}
- 
-    foreach ($reference in $project.Object.References)
-    {
-        if ($asms -contains $reference.Name + ".dll")
-        {
-            if($reference.Name -eq "ImmutableClass") { continue }
-            if($reference.CopyLocal -eq $true)
-            {
-                $reference.CopyLocal = $false;
-            }
-        }
-    }
-}
 
 function UnlockWeaversXml($project)
 {
@@ -88,8 +44,4 @@ function UnlockWeaversXml($project)
 
 UnlockWeaversXml($project)
 
-RemoveForceProjectLevelHack $project
-
 Update-FodyConfig $package.Id.Replace(".Fody", "") $project
-
-Fix-ReferencesCopyLocal $package $project
