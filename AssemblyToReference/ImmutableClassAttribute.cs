b79a66b6d7df0a4519b6d511326e033b62434aec
Tom Ekstrom <tom@taiste.fi>
2016-02-16 11:49:30 +0200

    ImmutableClassAttribute target now set as Class

--- AssemblyToReference/ImmutableClassAttribute.cs
+++ AssemblyToReference/ImmutableClassAttribute.cs
@@ -1,11 +1,11 @@
-﻿using System;
+using System;
 
 namespace ImmutableClass
 {
     /// <summary>
     /// Specifies that this class stub will be extended into a fully usable immutable class
     /// </summary>
-    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property, Inherited = false)]
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
     public class ImmutableClassAttribute : Attribute
     {
     }
