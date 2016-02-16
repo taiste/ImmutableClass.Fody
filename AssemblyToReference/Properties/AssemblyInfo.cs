695d0589d8efd6fc55a908e4d7af4fd560ae64a5
Tom Ekstrom <tom@taiste.fi>
2016-02-16 11:49:51 +0200

    Update to AssemblyInfo

--- AssemblyToReference/Properties/AssemblyInfo.cs
+++ AssemblyToReference/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Taiste Oy")]
 [assembly: AssemblyProduct("ImmutableClass")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
